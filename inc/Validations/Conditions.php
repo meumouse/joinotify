cbeee2af4f39f6a94728a3e9b9165ebf2c9dad32
MeuMouse.com <contato@meumouse.com>
2025-07-11 11:38:12 -0300

    Fixed issue: Verification failed on check if order is paid

--- inc/Validations/Conditions.php
+++ inc/Validations/Conditions.php
@@ -9,7 +9,7 @@ defined('ABSPATH') || exit;
  * Conditions class
  * 
  * @since 1.0.0
- * @version 1.2.5
+ * @version 1.3.6
  * @package MeuMouse.com
  */
 class Conditions {
@@ -77,7 +77,7 @@ class Conditions {
      * Gets comparison value based on condition type and context
      *
      * @since 1.0.0
-     * @version 1.2.5
+     * @version 1.3.6
      * @param string $condition_type | Condition type (e.g. 'order_total', 'user_role')
      * @param array $payload | Payload data
      * @return mixed Returns the value for comparison or null if not found
@@ -123,7 +123,7 @@ class Conditions {
             'post_author'           => is_object( $context ) && isset( $context->post_author ) ? get_the_author_meta( 'ID', $context->post_author ) : null,
             'order_status'          => $context instanceof \WC_Order ? $context->get_status() : null,
             'order_total'           => $context instanceof \WC_Order ? $context->get_total() : null,
-            'order_paid'            => $context instanceof \WC_Order ? $context->is_paid() : null,
+            'order_paid'            => $context instanceof \WC_Order ? ( $context->is_paid() ? 'true' : 'false' ) : null,
             'products_purchased'    => $context instanceof \WC_Order ? array_values( array_map( fn( $item ) => $item->get_product_id(), $context->get_items('line_item') ) ) : null,
             'customer_email'        => $context instanceof \WC_Order ? $context->get_billing_email() : null,
             'refund_amount'         => $context instanceof \WC_Order_Refund ? abs( $context->get_amount() ) : ( $context instanceof \WC_Order ? $context->get_total_refunded() : null ),
@@ -144,7 +144,7 @@ class Conditions {
      * Check condition
      * 
      * @since 1.0.0
-     * @version 1.2.2
+     * @version 1.3.6
      * @param string $condition | Condition type
      * @param mixed $value | Value for check
      * @param mixed $value_compare | Optional value for compare with $value
@@ -177,6 +177,11 @@ class Conditions {
                 case 'not_contain':
                     return empty( array_intersect( (array) $value, $expected_product_ids ) ); // No ID should be present
             }
+        }
+
+        // Check if the value is a boolean and the value_compare is a string
+        if ( is_bool( $value ) && is_string( $value_compare ) ) {
+            $value_compare = filter_var( $value_compare, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE );
         }
     
         // Standard cases for other conditions
