1f05dbe4e3fc2b5ad269bc649b67f99fb23a6567
MeuMouse.com <contato@meumouse.com>
2026-09-03 01:08:40 -0300

    Set release 2.4.0

--- joinotify.php
+++ joinotify.php
@@ -6,7 +6,7 @@
  * Plugin URI: 				https://meumouse.com/plugins/joinotify/
  * Author: 					MeuMouse.com
  * Author URI: 				https://meumouse.com/
- * Version: 				2.3.4
+ * Version: 				2.4.0
  * Requires at least: 		7.0
  * Requires PHP: 			8.1.0
  * Tested up to: 			7.1
@@ -33,7 +33,7 @@ if ( file_exists( $joinotify_autoload ) ) {
 	require_once $joinotify_autoload;
 }
 
-$joinotify_plugin_version = '2.3.4';
+$joinotify_plugin_version = '2.4.0';
 
 // Flag a fresh activation so the first admin request opens the setup wizard.
 // The wizard is where the site owner picks a country, connects the Joinotify
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: whatsapp, automation, woocommerce, notifications, workflow
 Requires at least: 7.0
 Tested up to: 7.1
 Requires PHP: 8.1.0
-Stable tag: 2.3.4
+Stable tag: 2.4.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
